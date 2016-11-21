7943acc1e6f9600e38a7e299654c82c099a3b7af
github username <github email address>
2016-11-21 13:52:20 -0500

    Done.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,34 @@
 // define your actions here!
+export function addMovie(movie){
+  return {
+    type: 'ADD_MOVIE',
+    movie:{
+      title: movie.title,
+      year: movie.year
+    }
+  };
+}
+
+export function deleteMovie(movie){
+  return {
+    type: 'DELETE_MOVIE',
+    movie:{
+      title: movie.title,
+      year: movie.year
+    }
+  };
+}
+
+export function updateUsername(newname){
+  return {
+    type: 'UPDATE_USERNAME',
+    username: newname
+  };
+}
+
+export function resetUsername(){
+  return{
+    type: 'RESET_USERNAME',
+    username: 'default'
+  };
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 const app = "I don't do much.";
 console.log(app);
+
+import addMovie from './actions/index'
